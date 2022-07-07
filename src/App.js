9a2ecd8fc15735cb26043b2f025f2d71adfce292
Abhay Gupta <adnorindia@gmail.com>
2022-07-07 19:57:59 +0530

    client files created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
 
 function App() {
   return (
+    // we will use BEM class naming convention
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        Let's build bubble.
+        <Header/>
+        {/* cards */}
+        {/* functions in button*/}
+
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+        <h2>I am Header.</h2>
+    </div>
+  )
+}
+
+export default Header
